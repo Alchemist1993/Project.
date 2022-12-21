4b58b3320ef5752553bb5620f31103917355fe18
Roman Amiraslanov <RomaAmiraslanov.ra@gmail.com>
2022-12-21 14:33:38 +0200

    Commit

---
+++ Project/src/routes/router.js
@@ -0,0 +1,16 @@
+import PAGES from "../models/pageModel.js";
+import { HOME_PAGE, ABOUT_PAGE } from "../services/domService.js";
+
+export const onChangePage = (page) => {
+  HOME_PAGE.className = "d-none";
+  ABOUT_PAGE.className = "d-none";
+
+  switch (page) {
+    case PAGES.HOME:
+      HOME_PAGE.className = "d-block";
+      break;
+    case PAGES.ABOUT:
+      ABOUT_PAGE.className = "d-block";
+      break;
+  }
+};
